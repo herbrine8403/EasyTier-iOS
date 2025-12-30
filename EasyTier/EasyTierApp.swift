6c2aad1c1faa1c24362d59e7b83f38d0630467e1
Chenx Dust <chenx_dust@outlook.com>
2025-12-30 14:39:01 +0800

    Initial Commit

---
+++ EasyTier/EasyTierApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyTierApp.swift
+//  EasyTier
+//
+//  Created by Chenx Dust on 2025/12/30.
+//
+
+import SwiftUI
+
+@main
+struct EasyTierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
